915e84f12ea2a015ffa734d65fd3a89203645352
Maatheusrosa <117786396+Maatheusrosa@users.noreply.github.com>
2023-03-28 16:06:48 -0300

    adicionando arquivos

---
+++ mousepadcompany/prot.js
@@ -0,0 +1,33 @@
+var sec_escondida = document.getElementById('escondida')
+var collections = document.getElementById('collections')
+
+var fechar = document.getElementById('fechar')
+var fotos = document.getElementById('imgl1')
+
+function clicou() {
+    if (sec_escondida.style.opacity = 'none') {
+        sec_escondida.style.display = 'block'
+    }
+}
+
+function fechou() {
+    if (sec_escondida.style.display = 'block') {
+        sec_escondida.style.display = 'none'
+    }
+}
+
+var fecharsim = document.getElementById('shadow')
+
+function fecharFora() {
+    if (fecharsim.style.display = 'block') {
+        sec_escondida.style.display = 'none'
+    }
+}
+
+
+
+
+
+
+
+
